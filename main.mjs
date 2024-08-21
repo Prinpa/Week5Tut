f1fa146e1342875b9d341a26d815fe124da37f51
Pat Dowd <pa77y04@gmail.com>
2024-08-21 19:50:56 +1000

    fixed

--- main.mjs
+++ main.mjs
@@ -1,6 +1,5 @@
 import { reverse } from "./reverse.mjs"
 
 // main goes here
-console.log(input)
-
+console.log("this is a really long testing string")
 console.log(reverse("this is a really long testing string"))
